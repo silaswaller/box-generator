b2a6b83d6718c358cc802a8c44c3ad58d50949ea
Silas Waller <silaswaller1@gmail.com>
2022-03-26 13:36:28 -0600

    commit1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
+  const [boxColor, setBoxColor] = useState("");
+  const [size, setSize] = useState("")
+  const [colorArr, setColorArr] = useState([]);
+  const addColorToArr = (e) => {
+    const newObj = {
+      color: boxColor,
+      size: size,
+    };
+    console.log('new state object', newObj);
+    e.preventDefault();
+    setColorArr([...colorArr, newObj]); 
+    this.setState({boxColor: ""});
+  }
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={addColorToArr}>
+        <div>
+          <label htmlFor="color">Color </label>
+          <input 
+          type="text" 
+          id="color" 
+          value={boxColor}
+          onChange={e => setBoxColor(e.target.value)} />
+        
+          <label>Size <input 
+          type="text"
+          onChange={e => setSize(e.target.value)}/></label>
+          <button type="submit" id="submit">SUBMIT</button>
+        </div>
+        
+      </form>
+      <h1>DISPLAY BOXES</h1>
+      {colorArr.map((obj, index) =>  {
+        return ( 
+        <div 
+          key={index}
+          style={{
+            height: obj.size + "px", 
+            width: obj.size + "px", 
+            backgroundColor: obj.color,
+            display: "inline-block",
+            marginLeft: "20px",
+          }} 
+        ></div>
+        );
+      })}
     </div>
   );
 }
